8137ff6414599021da8a5d4f0360022cf271e7e9
Muhammad__Umer <160643168+Mr-7-Umer@users.noreply.github.com>
2024-03-03 13:49:11 +0500

    Add files via upload
    json object in typescript in easy way

---
+++ json.js
@@ -0,0 +1,6 @@
+var details = {
+    "name": "Muhammad Umer",
+    "age": 16
+};
+console.log("My name is : " + details.name);
+console.log("My age is : " + details.age);
---
+++ json.ts
@@ -0,0 +1,8 @@
+// Json object (Javascript object notation)
+// is a text base data format which is use to store and transfer data among server and web applications 
+let details ={
+ "name" : "Muhammad Umer",
+"age" : 16
+}
+console.log("My name is : "+details.name);
+console.log("My age is : "+details.age);
